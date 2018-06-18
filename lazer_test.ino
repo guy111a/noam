7865a883b41bd802f4b39d0132952d750e24b952
guy <avraham.guy@gmail.com>
2018-06-18 18:20:32 +0300

    11

--- lazer_test.ino
+++ lazer_test.ino
@@ -17,3 +17,57 @@ digitalWrite(pin, LOW);
 delay(500) ;
 
 }
+
+
+/*
+
+int led = 13 ;
+int trigPin = A1 ;
+int echoPin = A0 ;
+int laser = A3 ;
+
+
+void setup() {
+  Serial.begin (9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(led, OUTPUT);
+  pinMode(laser, OUTPUT);
+}
+
+void loop() {
+  long duration, distance;
+  digitalWrite(trigPin, LOW);  // Added this line
+  delayMicroseconds(2); // Added this line
+  digitalWrite(trigPin, HIGH);
+  //  delayMicroseconds(1000); - Removed this line
+  delayMicroseconds(10); // Added this line
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration / 2) / 29.1;
+  if (distance < 20) {  // This is where the LED On/Off happens
+    digitalWrite(led, LOW);
+  }
+  else {
+    digitalWrite(led, HIGH);
+  }
+  if (distance >= 200 || distance <= 0) {
+    Serial.println("Out of range");
+  }
+  else {
+    Serial.print(distance);
+    Serial.println(" cm");
+  }
+  if (distance >= 0 && distance <= 5) {
+    Serial.println("laser canon");
+    digitalWrite(laser, HIGH);
+  }
+  else {
+    digitalWrite(laser, LOW);
+
+  }
+
+  delay(500);
+}
+
+ */
