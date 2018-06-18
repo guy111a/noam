cb3e7c71d3fbbdd6b3d49ebf1eec53e97edf00e4
guy <avraham.guy@gmail.com>
2018-06-18 18:03:16 +0300

    noam

---
+++ lazer_test.ino
@@ -0,0 +1,19 @@
+
+
+int pin = A0 ;
+
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(pin, OUTPUT) ;
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(pin, HIGH);
+delay(500) ;
+digitalWrite(pin, LOW);
+delay(500) ;
+
+}
